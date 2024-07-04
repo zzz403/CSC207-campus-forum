d599502dbc27644be3b04d7c182bcccf520797eb
Zirui Liu <zliu0312@gmail.com>
2024-07-04 19:28:04 -0400

    constructor image and info part infomation flow done

--- src/main/java/com/imperial/academia/view/ProfileView.java
+++ src/main/java/com/imperial/academia/view/ProfileView.java
@@ -1,13 +1,18 @@
 package com.imperial.academia.view;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.imperial.academia.interface_adapter.profile.ProfileController;
+import com.imperial.academia.interface_adapter.profile.ProfileState;
 import com.imperial.academia.interface_adapter.profile.ProfileViewModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileView extends JPanel {
     public final String viewName = "profile";
+    private final Map<String, ImageIcon> imageCache= new HashMap<>();
     // Constructor
     public ProfileView(ProfileController profileController, ProfileViewModel profileViewModel) {
         super(new BorderLayout());  // Set the layout of ProfileView to BorderLayout for simplicity
@@ -18,7 +23,7 @@ public class ProfileView extends JPanel {
         JPanel topPanel = new JPanel(new BorderLayout());
         topPanel.setPreferredSize(new Dimension(400, 100));
 
-        //TODO change default image to user image
+        // default image
         JPanel avatarPanel = new JPanel(new BorderLayout());
         avatarPanel.setBackground(new Color(255, 225, 120));
         ImageIcon originalIcon = new ImageIcon("resources/avatar/default_avatar.png");
@@ -28,15 +33,25 @@ public class ProfileView extends JPanel {
         avatarPanel.add(imageLabel);
         topPanel.add(avatarPanel, BorderLayout.WEST);
 
-        //TODO change default info to user info
         JPanel infoPanel = new JPanel(new GridLayout(0, 1));
         infoPanel.setBackground(new Color(255, 225, 120));
-        JLabel nameLabel = new JLabel("          UserName: UserID");
-        nameLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        JLabel nameLabel = new JLabel("          UserName: UserName");// TODO need fix
+        nameLabel.setFont(new Font("Arial", Font.BOLD, 20));
         JLabel emailLabel = new JLabel("          Email: xxx@mail.com");
         emailLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        JLabel idLabel = new JLabel("          User ID: UserId");
+        idLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        JLabel roleLabel = new JLabel("          Role : ");
+        roleLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        JLabel registrationDateLabel = new JLabel("          Member since ");
+        registrationDateLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+
         infoPanel.add(nameLabel, BorderLayout.CENTER);
         infoPanel.add(emailLabel, BorderLayout.CENTER);
+        infoPanel.add(idLabel, BorderLayout.CENTER);
+        infoPanel.add(roleLabel, BorderLayout.CENTER);
+        infoPanel.add(registrationDateLabel, BorderLayout.CENTER);
+
         topPanel.add(infoPanel, BorderLayout.CENTER);
 
         // TODO change bottom to user's post
@@ -64,5 +79,31 @@ public class ProfileView extends JPanel {
         mainPanel.add(scrollPane, constraints);
 
         this.add(mainPanel, BorderLayout.CENTER);  // Add mainPanel to ProfileView
+
+        profileViewModel.addPropertyChangeListener(evt ->{
+            if ("state".equals((evt.getPropertyName()))){
+                ProfileState state = (ProfileState) evt.getNewValue();
+                // set the new image
+                if (imageCache.containsKey(state.getAvatarUrl())){
+                    imageLabel.setIcon(imageCache.get(state.getAvatarUrl()));
+                }else{
+                ImageIcon UserIcon = new ImageIcon(state.getAvatarUrl());
+                Image resizedUserImage = UserIcon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+                ImageIcon resizedUserIcon = new ImageIcon(resizedUserImage);
+                imageLabel.setIcon(resizedUserIcon);
+                imageCache.put(state.getAvatarUrl(), resizedUserIcon);
+                }
+
+                // set the user info
+                nameLabel.setText("          UserName: " + state.getUsername());
+                emailLabel.setText("          Email: " + state.getEmail());
+                idLabel.setText("          User ID: " + state.getId());
+                roleLabel.setText("          Role : " + state.getRole());
+                registrationDateLabel.setText("          Member since " + state.getRegistrationDate().toString().substring(0,10));
+                //TODO  date formate need to be fixed
+//                state.getRegistrationDate();
+                // TODO info and post need to be done
+            }
+        });
     }
 }
