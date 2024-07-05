44d0e829666d110fdef313d4268efc36d5b1b7f3
Zhengyu-Joey-Wang <XDwoshiyingyan@gmail.com>
2024-07-04 20:00:19 -0400

    fix conflict

--- src/main/java/com/imperial/academia/app/usecase_factory/TopNavigationBarUseCaseFactory.java
+++ src/main/java/com/imperial/academia/app/usecase_factory/TopNavigationBarUseCaseFactory.java
@@ -15,22 +15,46 @@ import com.imperial.academia.use_case.session.SessionInputBoundary;
 import com.imperial.academia.use_case.session.SessionInteractor;
 import com.imperial.academia.view.components.TopNavigationBar;
 
-public class TopNavigationBarUseCaseFacory {
+/**
+ * Factory class for creating instances related to the Top Navigation Bar use case.
+ */
+public class TopNavigationBarUseCaseFactory {
+
+    /** Singleton instance of the top navigation bar controller. */
     private static TopNavigationBarController topNavigationBarController = null;
     
-    private TopNavigationBarUseCaseFacory(){}
+    /** Prevents instantiation of this utility class. */
+    private TopNavigationBarUseCaseFactory() {}
 
-    public static TopNavigationBar create(ViewManagerModel viewManagerModel,TopNavigationBarViewModel topNavigationBarViewModel, JFrame frame) throws IOException{
+    /**
+     * Creates a new instance of {@link TopNavigationBar} with the specified view manager model,
+     * top navigation bar view model, and frame.
+     * 
+     * @param viewManagerModel the view manager model
+     * @param topNavigationBarViewModel the top navigation bar view model
+     * @param frame the frame to which the top navigation bar will be added
+     * @return a new instance of {@link TopNavigationBar}
+     * @throws IOException if an I/O error occurs
+     */
+    public static TopNavigationBar create(ViewManagerModel viewManagerModel, TopNavigationBarViewModel topNavigationBarViewModel, JFrame frame) throws IOException {
         if (topNavigationBarController == null) {
-            topNavigationBarController = createController(viewManagerModel,topNavigationBarViewModel);
+            topNavigationBarController = createController(viewManagerModel, topNavigationBarViewModel);
         }
-        return new TopNavigationBar(topNavigationBarController,topNavigationBarViewModel, frame);
+        return new TopNavigationBar(topNavigationBarController, topNavigationBarViewModel, frame);
     }
 
-    private static TopNavigationBarController createController(ViewManagerModel viewManagerModel ,TopNavigationBarViewModel topNavigationBarViewModel){
-        ChangeViewOutputBoundary changeViewPresenter = new TopNavigationBarPresenter(viewManagerModel,topNavigationBarViewModel);
+    /**
+     * Creates a new instance of {@link TopNavigationBarController} with the specified view manager model
+     * and top navigation bar view model.
+     * 
+     * @param viewManagerModel the view manager model
+     * @param topNavigationBarViewModel the top navigation bar view model
+     * @return a new instance of {@link TopNavigationBarController}
+     */
+    private static TopNavigationBarController createController(ViewManagerModel viewManagerModel, TopNavigationBarViewModel topNavigationBarViewModel) {
+        ChangeViewOutputBoundary changeViewPresenter = new TopNavigationBarPresenter(viewManagerModel, topNavigationBarViewModel);
         ChangeViewInputBoundary changeViewInteractor = new ChangeViewInteractor(changeViewPresenter);
         SessionInputBoundary sessionInputBoundary = new SessionInteractor();
-        return new TopNavigationBarController(changeViewInteractor,sessionInputBoundary);
+        return new TopNavigationBarController(changeViewInteractor, sessionInputBoundary);
     }
 }
