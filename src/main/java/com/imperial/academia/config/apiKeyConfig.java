a593a79688ca11030f60867774f4b88ca2c904a2
AugustZ <zhengzhongze4@gmail.com>
2024-07-07 23:28:29 -0400

    Update .gitignore to ignore apiKeyConfig.java and remove it from index

--- src/main/java/com/imperial/academia/config/apiKeyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.imperial.academia.config;
-
-public class apiKeyConfig {
-    private static final String MAP_BOX_API_KEY = "";
-
-    public static String getMapBoxApiKey() {
-        return MAP_BOX_API_KEY;
-    }
-}
