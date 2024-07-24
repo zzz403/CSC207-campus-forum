137c775d875e419879573b3296a4de175968ecf1
Zirui Liu <zliu0312@gmail.com>
2024-07-23 21:36:58 -0400

    profile presenter test init done

---
+++ src/test/java/com/imperial/academia/interface_adapter/profile/ProfilePresenterTest.java
@@ -0,0 +1,25 @@
+package com.imperial.academia.interface_adapter.profile;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfilePresenterTest {
+    private ProfileViewModel mockProfileViewModel;
+    @BeforeEach
+    public void init(){
+        mockProfileViewModel = new ProfileViewModel();
+
+    }
+
+    @Test
+    void present() {
+
+    }
+
+    @Test
+    void presentError() {
+    }
+}
