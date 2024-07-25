fde150317bf13b9db52eefe228babd9dea5a0be7
Zirui Liu <zliu0312@gmail.com>
2024-07-25 02:52:15 -0400

    edit viewModel done

---
+++ src/main/java/com/imperial/academia/interface_adapter/edit/EditPresenter.java
@@ -0,0 +1,7 @@
+package com.imperial.academia.interface_adapter.edit;
+
+import com.imperial.academia.use_case.edit.EditOutputBoundry;
+
+public class EditPresenter implements EditOutputBoundry {
+
+}
--- src/main/java/com/imperial/academia/interface_adapter/edit/EditViewModel.java
+++ src/main/java/com/imperial/academia/interface_adapter/edit/EditViewModel.java
@@ -1,5 +1,61 @@
 package com.imperial.academia.interface_adapter.edit;
 
-public class EditViewModel {
+import com.imperial.academia.interface_adapter.common.ViewModel;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class EditViewModel extends ViewModel {
+    public final String TITLE_LABEL = "edit";
+    public final String USERNAME_LABEL = "Choose username";
+    public final String PASSWORD_LABEL = "Choose password";
+    public final String REPEAT_PASSWORD_LABEL = "Enter password again";
+    public final String EMAIL_LABEL = "Enter email";
+    public final String SIGNUP_BUTTON_LABEL = "Change";
+    public final String CANCEL_BUTTON_LABEL = "Cancel";
+
+    private  EditState state = new EditState();
+    private String errorMessage;
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public EditViewModel(){super("edit");};
+
+    public EditState getState(){return new EditState(state);}
+
+    public void setState(EditState state){
+        this.state = state;
+        support.firePropertyChange("state", null, state);// TODO why old value is null?
+    }
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        String oldMessage = this.errorMessage;
+        this.errorMessage = errorMessage;
+        support.firePropertyChange("errorMessage", oldMessage, errorMessage);
+    }
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    /**
+     * Fires a property change event to notify listeners of specific property changes.
+     *
+     * @param propertyName The name of the property that changed.
+     */
+    public void firePropertyChanged(String propertyName) {
+        support.firePropertyChange(propertyName, null, this.state);
+    }
+
+    /**
+     * Adds a PropertyChangeListener to the listener list.
+     *
+     * @param listener The PropertyChangeListener to be added.
+     */
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
