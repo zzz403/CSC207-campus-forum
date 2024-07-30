737d5316756a7e2d239c55ccc15f6f6eea333e26
Zirui Liu <zliu0312@gmail.com>
2024-07-30 17:30:20 -0400

    edit controller test done

---
+++ src/test/java/com/imperial/academia/interface_adapter/edit/EditControllerTest.java
@@ -0,0 +1,58 @@
+package com.imperial.academia.interface_adapter.edit;
+
+import com.imperial.academia.app.UsecaseFactory;
+import com.imperial.academia.use_case.changeview.ChangeViewInputBoundary;
+import com.imperial.academia.use_case.edit.EditInputBoundary;
+import com.imperial.academia.use_case.edit.EditInputData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class EditControllerTest {
+private EditInputBoundary mockEditInteractor;
+private ChangeViewInputBoundary mockChangeViewInteractor;
+private EditController editController;
+
+    @BeforeEach
+    public void init(){
+        mockEditInteractor = mock(EditInputBoundary.class);
+        mockChangeViewInteractor = mock(ChangeViewInputBoundary.class);
+
+        try (MockedStatic<UsecaseFactory> mockedStatic = Mockito.mockStatic(UsecaseFactory.class)){
+            mockedStatic.when(UsecaseFactory::getEditInteractor).thenReturn(mockEditInteractor);
+            mockedStatic.when(UsecaseFactory::getChangeViewInteractor).thenReturn(mockChangeViewInteractor);
+
+            editController = new EditController();
+        }
+    }
+
+    @Test
+    void execute() {
+        editController.execute();
+        verify(mockEditInteractor).execute();
+    }
+
+    @Test
+    void update() {
+        editController.update("", "","","","");
+        verify(mockEditInteractor).update(any(EditInputData.class));
+    }
+
+    @Test
+    void cancel() {
+        editController.cancel();
+        verify(mockChangeViewInteractor).changeView("profile");
+    }
+
+    @Test
+    void changeAvatar() {
+        editController.changeAvatar(1, new File(""));
+        verify(mockEditInteractor).changeAvatar(1,new File(""));
+    }
+}
